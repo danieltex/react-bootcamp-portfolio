fead6c035c89357296c8d0657b65d7c4650eb4ba
Daniel T. Santos <danieltex@gmail.com>
2019-09-11 10:05:20 -0300

    New component Project

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 import PROJECTS from './data/projects';
 
+class Project extends Component {
+    render() {
+        console.log("this.props", this.props);
+        const { title, image, description, link } = this.props.project;
+        return (
+            <div style={{ display: 'inline-block', width: 300, margin: 10}}>
+                <h3>{title}</h3>
+                <img src={image} alt='profile' style={{ width: 200, height: 120 }}/>
+                <p>{description}</p>
+                <a href={link}>{link}</a>
+            </div>
+        );
+    }
+}
+
 class Projects extends Component {
     render() {
         return (
@@ -8,8 +23,8 @@ class Projects extends Component {
                 <h2>Highlighted Projects</h2>
                 <div>
                     {
-                        PROJECTS.map((project) => 
-                            <div key={project.id}>{project.title}</div>
+                        PROJECTS.map(
+                            (project) => <Project key={project.id} project={project} />
                         )
                     }
                 </div>
