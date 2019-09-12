121152008fa21f3adba4285094587338f1b071ce
Daniel T. Santos <danieltex@gmail.com>
2019-09-12 17:26:56 -0300

    Adds social profiles

---
+++ src/data/socialProfiles.js
@@ -0,0 +1,27 @@
+import twitter_icon from '../assets/twitter_icon.png';
+import linkedin_icon from '../assets/linkedin_icon.png';
+import github_icon from '../assets/github_icon.png';
+
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        title: 'LinkedIn logo',
+        link: 'https://www.linkedin.com/in/danieltex/',
+        image: linkedin_icon
+    },
+    {
+      id: 2,
+      title: 'Github logo',
+      link: 'https://github.com/danieltex',
+      image: github_icon
+    },
+    {
+      id: 3,
+      title: 'Twitter logo',
+      link: 'https://twitter.com/danieltex',
+      image: twitter_icon
+    }
+]
+
+export default SOCIAL_PROFILES;
