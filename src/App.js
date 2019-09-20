3dee2887b3717ca256cb3f95e81e51d9fa751bc4
Daniel T. Santos <danieltex@gmail.com>
2019-09-20 18:19:08 -0300

    Title with fade

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Projects from './Projects';
 import SocialProfiles from './SocialProfiles';
+import Title from './Title';
+
 import profileImage from './assets/profile.jpg';
 
 class App extends Component {
@@ -29,12 +31,13 @@ class App extends Component {
                 <img src={profileImage} alt='profile' className='profile-image' />
                 <h1>Hello!</h1>
                 <p>My name is Daniel Santos.</p>
+                <Title />
                 <p>Mauris sit amet massa vitae tortor condimentum lacinia. Blandit aliquam etiam erat velit scelerisque in dictum non consectetur. Sit amet venenatis urna cursus. Maecenas accumsan lacus vel facilisis volutpat est velit egestas. Nibh sit amet commodo nulla facilisi. Nibh sit amet commodo nulla facilisi nullam vehicula. Suspendisse potenti nullam ac tortor vitae. Sit amet mauris commodo quis imperdiet massa tincidunt. Scelerisque viverra mauris in aliquam sem fringilla ut. In metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Tellus mauris a diam maecenas.</p>
                 {bio}
-               <hr />
-               <Projects />
-               <hr />
-               <SocialProfiles />
+                <hr />
+                <Projects />
+                <hr />
+                <SocialProfiles />
             </div>
         );
     }
