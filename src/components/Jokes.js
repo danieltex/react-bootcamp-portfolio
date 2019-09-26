5b326359aad6808e6c3e38e8f965e039aa3faa8e
Daniel T. Santos <danieltex@gmail.com>
2019-09-26 13:16:38 -0300

    More jokes

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,21 +1,39 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+
+const Joke = ({ joke: { setup, punchline } }) => (
+    <p style={{ margin: 20 }}>{setup} <em>{punchline}</em></p>
+)
 
 class Jokes extends Component {
-    state = { joke: {} };
+    state = { joke: {}, jokes: [] };
 
     componentDidMount() {
         fetch('https://official-joke-api.appspot.com/random_joke')
-        .then(res => res.json())
-        .then(json => this.setState({ joke: json }))
-        .catch(err => console.log(err.message));
+            .then(res => res.json())
+            .then(json => this.setState({ joke: json }))
+            .catch(err => console.log(err.message));
     }
 
-    render() {
-        const { setup, punchline} = this.state.joke;
+    fetchJokes = () => {
+        fetch('https://official-joke-api.appspot.com/random_ten')
+            .then(res => res.json())
+            .then(json => this.setState({ jokes: json }))
+            .catch(err => console.log(err.message));
+    }
+
+    render() {        
         return (
             <div>
                 <h2>Highlighted joke</h2>
-                <p>{setup} <em>{punchline}</em></p>
+                <Joke joke={this.state.joke}></Joke>
+                <hr />
+                <h3>Want more ten jokes?</h3>
+                <button onClick={ this.fetchJokes }>Click me!</button> 
+                {
+                    this.state.jokes.map(
+                        joke => <Joke key={ joke.id } joke={joke}></Joke>
+                    )
+                }               
             </div>
         );
     }
